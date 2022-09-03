4db26a83a9fec17b5910768154dba1386972eb0b
Lovish Prabhakar <2020010006574@gndu.ac.in>
2022-09-03 14:56:34 +0530

    Adding LinkedIN

--- Code/js/common.js
+++ Code/js/common.js
@@ -3,4 +3,4 @@ var header_list = document.querySelector("#header_list");
 
 header_info.innerHTML =
     "AlooZe collects data from around the world and organises it into networks of user touchpoints around certain themes, producing user-friendly results. There will be some exciting new features coming soon.";
-header_list.innerHTML = `<li><a target="_blank" href="https://www.linkedin.com/in/robin-rajesh-a43360191" class="text-white"><i class="bi bi-linkedin"></i> Follow on LinkedIn</a></li>`;
+header_list.innerHTML = `<li><a target="_blank" href="https://www.linkedin.com/in/lovish-prabhakar-558855147/" class="text-white"><i class="bi bi-linkedin"></i> Follow on LinkedIn</a></li>`;
